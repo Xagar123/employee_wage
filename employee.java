4bd810a6e4e8381f7445e3c1610e19489d286d69
Xagar123 <sagarcoc2000@gmail.com>
2022-09-06 08:10:42 +0530

    UC2 added

--- employee.java
+++ employee.java
@@ -2,11 +2,20 @@ public class employee {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
         int present = 1;
+        int Emp_rate = 20;
+        int Emp_hour = 0;
+        int Emp_wage = 0;
         int employe_check = (int) (Math.floor(Math.random() * 10) % 2);
         if (employe_check == present) {
-            System.out.println("Employe is present" );
+            System.out.println("Employe is present");
+            Emp_hour = 8;
         } else {
-            System.out.println("Employee is absent" );
+            System.out.println("Employee is absent");
+            Emp_hour = 0;
         }
+        Emp_wage = Emp_hour * Emp_rate;
+        System.out.println("Emp_Wage : " + Emp_wage);
     }
 }
+    
+
