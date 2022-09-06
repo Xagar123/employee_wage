facd6915d94dcc5446d8f27ea0c845d27e9e7aca
Xagar123 <sagarcoc2000@gmail.com>
2022-09-06 08:17:58 +0530

    UC3 added

--- employee.java
+++ employee.java
@@ -5,16 +5,22 @@ public class employee {
         int Emp_rate = 20;
         int Emp_hour = 0;
         int Emp_wage = 0;
-        int employe_check = (int) (Math.floor(Math.random() * 10) % 2);
+        int part_time = 2;
+        int employe_check = (int) (Math.floor(Math.random() * 10) % 3);
         if (employe_check == present) {
-            System.out.println("Employe is present");
+            System.out.println("Employe is present" );
             Emp_hour = 8;
-        } else {
-            System.out.println("Employee is absent");
+        }
+        else if(part_time == employe_check){
+            System.out.println("Employee is Part time");
+            Emp_hour = 4;
+        }
+        else {
+            System.out.println("Employee is absent" );
             Emp_hour = 0;
         }
         Emp_wage = Emp_hour * Emp_rate;
-        System.out.println("Emp_Wage : " + Emp_wage);
+        System.out.println("Emp_Wage : "+ Emp_wage);
     }
 }
     
