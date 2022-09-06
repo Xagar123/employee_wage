2ae1ccc5b5d6b76e4bda8b43086eaa4f62c7e7a2
Xagar123 <sagarcoc2000@gmail.com>
2022-09-06 08:29:39 +0530

    UC4 Added

--- employee.java
+++ employee.java
@@ -1,24 +1,29 @@
 public class employee {
+    public static final int present = 1;
+    public static final int part_time = 2;
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation Program");
-        int present = 1;
+        System.out.println("Welcome to employee wage progrsm");
+
         int Emp_rate = 20;
         int Emp_hour = 0;
         int Emp_wage = 0;
-        int part_time = 2;
-        int employe_check = (int) (Math.floor(Math.random() * 10) % 3);
-        if (employe_check == present) {
-            System.out.println("Employe is present" );
-            Emp_hour = 8;
+
+        int employe_check = (int) Math.floor(Math.random() * 10) % 3;
+        switch (employe_check){
+            case present:
+                System.out.println("Employee is full time");
+                Emp_hour = 8;
+                break;
+            case part_time:
+                System.out.println("Employee is part time");
+                Emp_hour = 4;
+                break;
+            default:
+                System.out.println("Employee is absent");
+                Emp_hour = 0;
+                break;
         }
-        else if(part_time == employe_check){
-            System.out.println("Employee is Part time");
-            Emp_hour = 4;
-        }
-        else {
-            System.out.println("Employee is absent" );
-            Emp_hour = 0;
-        }
+
         Emp_wage = Emp_hour * Emp_rate;
         System.out.println("Emp_Wage : "+ Emp_wage);
     }
